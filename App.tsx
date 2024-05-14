be594b8caf2edc71f838fc7458e1d97362517bcb
hrushii <hrushiop@gmail.com>
2024-05-14 10:52:38 +0530

    Added app icon and status bar styling

--- App.tsx
+++ App.tsx
@@ -92,7 +92,7 @@ function App(): React.JSX.Element {
             <DebugInstructions />
           </Section>
           <Section title="Learn More">
-            Read the docs to discover what to do next:
+            Read the docs to discover what to do next
           </Section>
           <LearnMoreLinks />
         </View>
