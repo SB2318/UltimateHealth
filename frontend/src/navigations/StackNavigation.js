3ee708d9bfd9921ded10032dee7217e53aaef55a
sidharth2829 <111569459+sidharth2829@users.noreply.github.com>
2024-05-27 15:57:45 +0530

    Fixes #66
    Disabled the navigation of TabNavigation and handled the event of BackButton

--- frontend/src/navigations/StackNavigation.js
+++ frontend/src/navigations/StackNavigation.js
@@ -1,11 +1,32 @@
-import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
-import TabNavigation from "./TabNavigation";
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { BackHandler, Alert } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
+import TabNavigation from './TabNavigation';
 import SplashScreen from '../screens/SplashScreen';
 
 const Stack = createStackNavigator();
 
-const StackNavigation = () => {
+const StackNavigation = ({ navigation, route }) => {
+  useFocusEffect(
+    React.useCallback(() => {
+      const handleBackButton = () => {
+        if (route.name === 'SplashScreen') {
+          Alert.alert('Exit App', 'Are you sure you want to exit?', [
+            { text: 'Cancel', style: 'cancel' },
+            { text: 'OK', onPress: () => BackHandler.exitApp() },
+          ]);
+          return true;
+        }
+        return false;
+      };
+
+      const backHandler = BackHandler.addEventListener('hardwareBackPress', handleBackButton);
+
+      return () => backHandler.remove();
+    }, [route])
+  );
+
   return (
     <Stack.Navigator>
       <Stack.Screen
@@ -16,9 +37,10 @@ const StackNavigation = () => {
       <Stack.Screen
         name="TabNavigation"
         component={TabNavigation}
-        options={{ headerShown: false }}
+        options={{ headerShown: false,gestureEnabled: false}}
       />
     </Stack.Navigator>
   );
 };
+
 export default StackNavigation;
