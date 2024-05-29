a31e162e767d72a449c2b8c9822b8cc007cc0bea
Himanshu8850 <1130himanshu@gmail.com>
2024-05-29 10:35:37 +0530

    Added user login check

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -7,24 +7,18 @@
 
 import React from 'react';
 
-import {
-  Platform,
-  StatusBar,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {Platform, StatusBar, useColorScheme, View} from 'react-native';
 
-import {
-  Colors
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {PRIMARY_COLOR} from './src/Theme';
 import {NavigationContainer} from '@react-navigation/native';
 import StackNavigation from './src/navigations/StackNavigation';
 import TabNavigation from './src/navigations/TabNavigation';
 import LoginScreen from './src/screens/auth/LoginScreen';
-import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
-
-
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -36,21 +30,21 @@ function App(): React.JSX.Element {
   const BarStyle = Platform.OS === 'ios' ? 'dark-content' : 'light-content';
   return (
     <SafeAreaProvider>
-    <View style={{flex:1,backgroundColor:backgroundStyle.backgroundColor}}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : BarStyle}
-        backgroundColor={
-          isDarkMode ? backgroundStyle.backgroundColor : PRIMARY_COLOR
+      <View style={{flex: 1, backgroundColor: backgroundStyle.backgroundColor}}>
+        <StatusBar
+          barStyle={isDarkMode ? 'light-content' : BarStyle}
+          backgroundColor={
+            isDarkMode ? backgroundStyle.backgroundColor : PRIMARY_COLOR
+          }
+        />
+        {
+          <NavigationContainer>
+            <StackNavigation />
+          </NavigationContainer>
         }
-      />
-      { <NavigationContainer>
-        <StackNavigation />
-      </NavigationContainer> }
-    </View>
+      </View>
     </SafeAreaProvider>
   );
 }
-
-
 
 export default App;
--- frontend/src/screens/SplashScreen.jsx
+++ frontend/src/screens/SplashScreen.jsx
@@ -1,13 +1,27 @@
-import React, { useEffect } from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
-import { ON_PRIMARY_COLOR, PRIMARY_COLOR, SECONDARY_COLOR } from '../Theme';
+import React, {useEffect} from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {ON_PRIMARY_COLOR, PRIMARY_COLOR, SECONDARY_COLOR} from '../Theme';
 import icon from '../assets/icon.png';
 
-const SplashScreen = ({ navigation }) => {
+const SplashScreen = ({navigation}) => {
+  const checkLoginStatus = async () => {
+    try {
+      const user = await AsyncStorage.getItem('user');
+      if (user) {
+        navigation.navigate('TabNavigation'); // Navigate to HomePage if user is logged in
+      } else {
+        navigation.navigate('LoginScreen'); // Navigate to LoginPage if user is not logged in
+      }
+    } catch (error) {
+      console.error('Error retrieving user data from storage', error);
+      navigation.navigate('LoginScreen'); // Navigate to LoginPage if there's an error
+    }
+  };
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      navigation.navigate('TabNavigation');
+      checkLoginStatus();
     }, 3000);
 
     return () => clearTimeout(timer);
@@ -20,7 +34,7 @@ const SplashScreen = ({ navigation }) => {
       <Text style={styles.text}>Health</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -31,8 +45,8 @@ const styles = StyleSheet.create({
   },
   icon: {
     width: 120,
-    height: 120, 
-    borderRadius:60,
+    height: 120,
+    borderRadius: 60,
     marginBottom: 10,
   },
   text: {
@@ -40,7 +54,7 @@ const styles = StyleSheet.create({
     // fontWeight: 'bold',
     color: ON_PRIMARY_COLOR,
     textAlign: 'center',
-    fontFamily:"Lobster-Regular"
+    fontFamily: 'Lobster-Regular',
   },
 });
 
