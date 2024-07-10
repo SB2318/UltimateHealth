4d46fce2baaf8bdffbf2ce079953363927d7070f
Nishant Kaushal <101548649+nishant0708@users.noreply.github.com>
2024-07-10 23:34:45 +0530

    updated (#159)

---
+++ backend/models/Articles.js
@@ -0,0 +1,78 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
+const articleSchema = new Schema({
+  _id: {
+    type: Number,
+    autoIncrement: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  authorName: {
+    type: String,
+    required: true,
+  },
+  authorId: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  summary: {
+    type: String,
+    required: true,
+  },
+  published_date: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  last_updated: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  tags: {
+    type: [String],
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: ['Draft', 'Published', 'Archived'],
+  },
+  imageUtils: {
+    type: [String],
+    required: true,
+  },
+  relatedBodyPart: {
+    type: [String],
+    required: true,
+  },
+  viewCount: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  likeCount: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  language: {
+    type: String,
+    required: true,
+    default: 'English',
+  },
+});
+
+// Apply the autoIncrement plugin to the schema
+articleSchema.plugin(AutoIncrement, { id: 'article_id_counter', inc_field: '_id' });
+
+const Article = mongoose.model('Article', articleSchema);
+
+module.exports = Article;
