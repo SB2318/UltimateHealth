c112fb3a3cd43f7872421b2d1ea4784a29386260
Nishant Kaushal <101548649+nishant0708@users.noreply.github.com>
2024-07-11 21:50:34 +0530

    updated

--- backend/controllers/articleController.js
+++ backend/controllers/articleController.js
@@ -60,6 +60,30 @@ module.exports.deleteArticle = async (req, res) => {
     res.status(500).json({ error: 'Error deleting article', details: error.message });
   }
 };
+module.exports.getArticleByTitle =   async (req, res) => {
+    try {
+      const article = await Article.findOne({ title: req.params.title });
+      if (!article) {
+        return res.status(404).json({ message: 'Article not found' });
+      }
+      res.status(200).json({ article });
+    } catch (error) {
+      res.status(500).json({ error: 'Error fetching article', details: error.message });
+    }
+  };
+  module.exports.getArticleByName =   async (req, res) => {
+      try {
+        const article = await Article.findOne({ name: req.params.name });
+        if (!article) {
+          return res.status(404).json({ message: 'Article not found' });
+        }
+        res.status(200).json({ article });
+      } catch (error) {
+        res.status(500).json({ error: 'Error fetching article', details: error.message });
+      }
+    }
+  
+
 
 
 /**** For Article Tags */
--- backend/routes/articleRoutes.js
+++ backend/routes/articleRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const articleController = require('../controllers/articleController');
+const authenticateToken = require('../middleware/authentcatetoken');
 
 /**** For Article Tags */
 router.post('/articles/tags', articleController.addNewTag);
@@ -9,11 +10,14 @@ router.put('/articles/tags/:id', articleController.updateTagById);
 router.delete('/articles/tags/:id', articleController.deleteArticleTagByIds);
 
 
+
 /**** For Article *****/
-router.post('/articles', articleController.createArticle); // Create a new article
-router.get('/articles', articleController.getAllArticles);// Get all articles
-router.get('/articles/:id', articleController.getArticleById);// Get an article by ID
-router.put('/articles/:id', articleController.updateArticle);// Update an article by ID
-router.delete('/articles/:id', articleController.deleteArticle);// Delete an article by ID
+router.post('/articles',authenticateToken, articleController.createArticle); // Create a new article
+router.get('/articles',authenticateToken, articleController.getAllArticles);// Get all articles
+router.get('/articles/:id',authenticateToken, articleController.getArticleById);// Get an article by ID
+router.put('/articles/:id',authenticateToken, articleController.updateArticle);// Update an article by ID
+router.delete('/articles/:id',authenticateToken, articleController.deleteArticle);// Delete an article by ID
+router.get('/articles/:title',authenticateToken, articleController.getArticleByTitle);// get article by title
+router.get('/articles/:name',authenticateToken, articleController.getArticleByName);// / get an article by name
 
 module.exports = router;
