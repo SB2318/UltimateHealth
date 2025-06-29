c3b2c9e4be85ae722f3a0bf9b140cc169506ef03
SB2318 <sb2318@it.jgec.ac.in>
2025-06-29 12:24:21 +0530

    --

--- frontend/src/helper/trackPlayerService.ts
+++ frontend/src/helper/trackPlayerService.ts
@@ -1,6 +1,7 @@
 import TrackPlayer from 'react-native-track-player';
 
 const trackService = async function () {
+    
   TrackPlayer.addEventListener('remote-play', () => TrackPlayer.play());
   TrackPlayer.addEventListener('remote-pause', () => TrackPlayer.pause());
 };
