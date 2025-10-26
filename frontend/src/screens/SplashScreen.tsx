f7094f486f4df0dd64398cec8ecd6d106774085c
SB2318 <sb2318@it.jgec.ac.in>
2025-10-26 12:24:10 +0530

    --publish--nehi--hoga--to--ap--sb-hi--likhenge--ki--sb2318--cheater--hai

--- frontend/src/screens/SplashScreen.tsx
+++ frontend/src/screens/SplashScreen.tsx
@@ -118,11 +118,11 @@ const SplashScreen = ({navigation}: SplashScreenProp) => {
   return (
     <View style={styles.container}>
       
-         <Image source={require('../../assets/images/icon.png')} style={styles.icon} />
+      <Image source={require('../../assets/images/icon.png')} style={styles.icon} />
          
       <Text style={styles.text}>Ultimate</Text>
 
-      
+
       <Text style={styles.text}>Health</Text>
     </View>
   );
