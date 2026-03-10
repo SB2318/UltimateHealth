79f5504fecef550a43b6c0c129ead9113d5a4486
SB2318 <sb2318@it.jgec.ac.in>
2026-03-10 21:56:58 +0530

    Add new hooks for fetching user statistics: likes, views, reads, and writes

---
+++ frontend/src/hooks/useGetMostViewedArticle.ts
@@ -0,0 +1,30 @@
+
+import { useQuery, UseQueryResult } from "@tanstack/react-query";
+import axios from "axios";
+import { ArticleData } from "../type";
+import { GET_MOSTLY_VIEWED } from "../helper/APIUtils";
+
+export const useGetAuthorMostViewedArticles = (
+  user_id: string,
+  userId?: string,
+  others?: boolean,
+  isConnected?: boolean
+): UseQueryResult<ArticleData[]> => {
+
+  return useQuery<ArticleData[]>({
+    queryKey: ["get-mostly-viewed-article", user_id, userId, others],
+
+    queryFn: async () => {
+   
+      const url = others
+        ? `${GET_MOSTLY_VIEWED}${userId}`
+        : `${GET_MOSTLY_VIEWED}${user_id}`;
+
+      const response = await axios.get(url);
+
+      return response.data as ArticleData[];
+    },
+
+    enabled: !!isConnected && !!(!userId && others) && !!(!user_id && !others),
+  });
+};
---
+++ frontend/src/hooks/useGetTotalWrites.ts
@@ -0,0 +1,30 @@
+
+import { useQuery, UseQueryResult } from "@tanstack/react-query";
+import axios from "axios";
+import { WriteStatus } from "../type";
+import { GET_TOTAL_WRITES } from "../helper/APIUtils";
+
+export const useGetTotalReads = (
+  user_id: string,
+  userId?: string,
+  others?: boolean,
+  isConnected?: boolean
+): UseQueryResult<WriteStatus> => {
+
+  return useQuery<WriteStatus>({
+    queryKey: ["get-total-writes", user_id, userId, others],
+
+    queryFn: async () => {
+   
+      const url = others
+        ? `${GET_TOTAL_WRITES}${userId}`
+        : `${GET_TOTAL_WRITES}${user_id}`;
+
+      const response = await axios.get(url);
+
+      return response.data as WriteStatus;
+    },
+
+    enabled: !!isConnected && !!(!userId && others) && !!(!user_id && !others),
+  });
+};
