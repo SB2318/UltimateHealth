552612eeb6a19dd3a7fdff2ffc5bd27e91d8d2c1
S. B. | Software Developer <87614560+SB2318@users.noreply.github.com>
2026-05-25 10:26:44 +0530

    --type--check--and--other--ci--fix

--- frontend/src/store/__tests__/dataSlice.test.ts
+++ frontend/src/store/__tests__/dataSlice.test.ts
@@ -1,3 +1,5 @@
+/// <reference types="jest" />
+
 import reducer, {
   setFilteredArticles,
   setSearchedArticles,
@@ -13,10 +15,17 @@ import reducer, {
   appendPodcasts,
   setaddedPodcastId,
   setRemovePlaylistId,
+  type DataState,
 } from '../dataSlice';
 
+type Article = DataState['article'];
+
+type Podcast = {
+  id: string;
+};
+
 describe('dataSlice reducers', () => {
-  const defaultArticle = {
+  const defaultArticle: Article = {
     _id: '',
     title: '',
     authorName: '',
@@ -40,10 +49,10 @@ describe('dataSlice reducers', () => {
     reviewer_id: undefined,
     contributors: [],
     pb_recordId: '',
-    language: "en-IN"
+    language: 'en-IN',
   };
 
-  const initialState = {
+  const initialState: DataState = {
     filteredArticles: [],
     searchedArticles: [],
     selectedTags: [],
@@ -61,86 +70,162 @@ describe('dataSlice reducers', () => {
   };
 
   it('should return the initial state', () => {
-    // @ts-ignore
     expect(reducer(undefined, { type: 'unknown' })).toEqual(initialState);
   });
 
   it('should handle setFilteredArticles', () => {
     const mockArticles = [{ _id: '1', title: 'Test' }];
-    const actual = reducer(initialState, setFilteredArticles({ filteredArticles: mockArticles }));
+
+    const actual = reducer(
+      initialState,
+      setFilteredArticles({ filteredArticles: mockArticles }),
+    );
+
     expect(actual.filteredArticles).toEqual(mockArticles);
   });
 
   it('should handle setSearchedArticles', () => {
     const mockArticles = [{ _id: '2', title: 'Test Search' }];
-    const actual = reducer(initialState, setSearchedArticles({ searchedArticles: mockArticles }));
+
+    const actual = reducer(
+      initialState,
+      setSearchedArticles({ searchedArticles: mockArticles }),
+    );
+
     expect(actual.searchedArticles).toEqual(mockArticles);
   });
 
   it('should handle setSelectedTags', () => {
     const mockTags = [{ name: 'Health' }];
-    const actual = reducer(initialState, setSelectedTags({ selectedTags: mockTags }));
+
+    const actual = reducer(
+      initialState,
+      setSelectedTags({ selectedTags: mockTags }),
+    );
+
     expect(actual.selectedTags).toEqual(mockTags);
   });
 
   it('should handle setSortType', () => {
-    const actual = reducer(initialState, setSortType({ sortType: 'recent' }));
+    const actual = reducer(
+      initialState,
+      setSortType({ sortType: 'recent' }),
+    );
+
     expect(actual.sortType).toEqual('recent');
   });
 
   it('should handle setSearchMode', () => {
-    const actual = reducer(initialState, setSearchMode({ searchMode: true }));
+    const actual = reducer(
+      initialState,
+      setSearchMode({ searchMode: true }),
+    );
+
     expect(actual.searchMode).toEqual(true);
   });
 
   it('should handle setArticle', () => {
-    const newArticle = { ...defaultArticle, title: 'New Article' };
-    const actual = reducer(initialState, setArticle({ article: newArticle }));
+    const newArticle: Article = {
+      ...defaultArticle,
+      title: 'New Article',
+    };
+
+    const actual = reducer(
+      initialState,
+      setArticle({ article: newArticle }),
+    );
+
     expect(actual.article).toEqual(newArticle);
   });
 
   it('should handle setTags', () => {
     const tags = [{ name: 'Tech' }];
-    const actual = reducer(initialState, setTags({ tags }));
+
+    const actual = reducer(
+      initialState,
+      setTags({ tags }),
+    );
+
     expect(actual.categories).toEqual(tags);
   });
 
   it('should handle setSuggestion', () => {
-    const actual = reducer(initialState, setSuggestion({ suggestion: 'Great post!' }));
+    const actual = reducer(
+      initialState,
+      setSuggestion({ suggestion: 'Great post!' }),
+    );
+
     expect(actual.suggestion).toEqual('Great post!');
   });
 
   it('should handle setSuggestionAccepted', () => {
-    const actual = reducer(initialState, setSuggestionAccepted({ selection: true }));
+    const actual = reducer(
+      initialState,
+      setSuggestionAccepted({ selection: true }),
+    );
+
     expect(actual.suggestionAccepted).toEqual(true);
   });
 
   it('should handle setSelectePodcastCategories', () => {
     const categories = ['Tech', 'Health'];
-    const actual = reducer(initialState, setSelectePodcastCategories(categories));
+
+    const actual = reducer(
+      initialState,
+      setSelectePodcastCategories(categories),
+    );
+
     expect(actual.selectedPodcastCategories).toEqual(categories);
   });
 
   it('should handle setPodcasts', () => {
-    const mockPodcasts = [{ _id: 'pod1' }];
-    const actual = reducer(initialState, setPodcasts(mockPodcasts));
+    const mockPodcasts: Podcast[] = [{ id: 'pod1' }];
+
+    const actual = reducer(
+      initialState,
+      setPodcasts(mockPodcasts),
+    );
+
     expect(actual.podcasts).toEqual(mockPodcasts);
   });
 
   it('should handle appendPodcasts', () => {
-    const mockState = { ...initialState, podcasts: [{ _id: 'pod1' } as any] };
-    const actual = reducer(mockState, appendPodcasts([{ _id: 'pod2' }]));
+    const mockState: DataState = {
+      ...initialState,
+      podcasts: [{
+        id: 'pod1',
+        title: '',
+        host: '',
+        imageUri: '',
+        likes: 0,
+        duration: 0
+      }],
+    };
+
+    const actual = reducer(
+      mockState,
+      appendPodcasts([{ id: 'pod2' }]),
+    );
+
     expect(actual.podcasts.length).toEqual(2);
-    expect(actual.podcasts[1]._id).toEqual('pod2');
+    expect(actual.podcasts[1].id).toEqual('pod2');
   });
 
   it('should handle setaddedPodcastId', () => {
-    const actual = reducer(initialState, setaddedPodcastId('pod123'));
+    const actual = reducer(
+      initialState,
+      setaddedPodcastId('pod123'),
+    );
+
     expect(actual.addedPodcastId).toEqual('pod123');
   });
 
   it('should handle setRemovePlaylistId', () => {
-    const actual = reducer(initialState, setRemovePlaylistId('playlist456'));
+    const actual = reducer(
+      initialState,
+      setRemovePlaylistId('playlist456'),
+    );
+
     expect(actual.removePlaylistId).toEqual('playlist456');
   });
 });
